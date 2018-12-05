1fe496f52736919c602ecb8c9e2cc6cd40529e39
Yang Zhang <zhangy405@yangs-mac-mini.local>
2018-12-05 10:36:50 -0800

    Initial Commit

---
+++ FBProfile/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FBProfile
+//
+//  Created by Yang Zhang on 12/5/18.
+//  Copyright © 2018 Yang Zhang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
